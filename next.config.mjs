381f668ff1fa1d7e3a6d3eff3805f724f69f342d
Jihad Hasan Masum <jihadhasan361@gmail.com>
2026-04-06 10:25:09 +0600

    disable

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  disable: "true",
+  disable: true, 
 });
 
 /** @type {import('next').NextConfig} */
